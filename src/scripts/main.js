ca6dbbd079291e5781e8bab1a53415a4f2e4f4ae
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-05-18 18:44:48 -0500

    completed form functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,24 +2,69 @@
 // import checkLoginStatus from './helpers/auth';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import axios from 'axios';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
-
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
-
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+const renderToDom = (divId, textToRender) => {
+  const selectedElement = document.querySelector(divId);
+  selectedElement.innerHTML = textToRender;
 };
 
-init();
+const getLyrics = () => new Promise((resolve, reject) => {
+  axios.get(`https://api.lyrics.ovh/v1/${document.querySelector('#artist').value}/${document.querySelector('#song').value}`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+const lyricsOnDom = (artist, song) => {
+  getLyrics(artist, song).then((response) => {
+    document.querySelector('#app').innerHTML = response.lyrics.replaceAll(
+      '\n',
+      '<br>'
+    );
+  });
+};
+
+const renderForm = () => {
+  const domString = `<form>
+          <div class="form-floating mb-3">
+            <input id="artist" class="form-control form-control-lg" type="text" placeholder="artist" id="artist" aria-label="artist" required>
+            <label class="label-background" for="artist">Artist Name</label>
+          </div>
+          
+          <div class="form-floating mb-3">
+            <input id="song" class="form-control form-control-lg" type="text" placeholder="song" id="song" aria-label="song" required>
+            <label class="label-background" for="song">Song Title</label>
+          </div>
+          
+  
+  <button id="submit" type="submit" class="btn btn-primary">Get Lyrics</button>
+</form>`;
+  renderToDom('#login-form-container', domString);
+};
+
+const buttonOnDom = () => {
+  const domString = `<button id="consoleButton" type="button" class="btn btn-primary"
+        style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+  Custom button
+</button>`;
+  renderToDom('#button-div', domString);
+};
+
+const eventListeners = () => {
+  document.querySelector('#login-form-container').addEventListener('submit', (e) => {
+    e.preventDefault();
+    getLyrics();
+    lyricsOnDom();
+    document.querySelector('#consoleButton').addEventListener('click', () => {
+      console.warn();
+    });
+  });
+};
+
+const startApp = () => {
+  buttonOnDom();
+  renderForm();
+  eventListeners();
+};
+
+startApp();
